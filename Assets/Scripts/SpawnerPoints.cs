e3ebeab52e59cd007e14bd8a7a341e0f048a1d69
darlinkion <49286517+darlinkion@users.noreply.github.com>
2022-02-08 20:53:31 +0300

    Привязал респаун к точкам

--- Assets/Scripts/SpawnerPoints.cs
+++ Assets/Scripts/SpawnerPoints.cs
@@ -6,27 +6,25 @@ public class SpawnerPoints : MonoBehaviour
 {
 
     [SerializeField] private GameObject _tempMob;
-    [SerializeField] private int _count;
-    [SerializeField] private float _radius;
+    [SerializeField] private int _numberMobs;
+    [SerializeField] private Transform[] _points;
 
     private void Start()
     {
         StartCoroutine(SpawnMob());
     }
 
-
     private IEnumerator SpawnMob()
     {
-        int stepAngly = 360 / _count;
         var waitForTwoSeconds = new WaitForSeconds(2);
 
-        for (int i = 0; i < _count; i++)
+        for (int i = 0; i < _numberMobs; i++)
         {
             GameObject newObject = Instantiate(_tempMob, new Vector3(0, 0, 0), Quaternion.identity);
 
             Transform tempObjectTransform = newObject.GetComponent<Transform>();
 
-            tempObjectTransform.position = new Vector3(_radius * (Mathf.Cos(stepAngly * i * Mathf.Deg2Rad)), 0, _radius * (Mathf.Sin(stepAngly * i * Mathf.Deg2Rad)));
+            tempObjectTransform.position = _points[Random.Range(0, _points.Length)].position;
 
             yield return waitForTwoSeconds;
         }
